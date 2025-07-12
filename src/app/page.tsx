e672cf43f76affe780219947e00b47af861601fc
dev-Nahian <nahian.bdcalling@gmail.com>
2025-07-12 15:55:38 +0600

    new product component fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ const HomePage = async () => {
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-[160px]">
         <h1 className="text-2xl">New Products</h1>
-        {/* <ProductList /> */}
+        <ProductList categoryId={process.env.NEW_PRODUCTS_CATEGORY_ID!} />
       </div>
     </div>
   );
